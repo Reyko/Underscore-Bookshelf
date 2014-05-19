9915d157f54f84373667a3e89eefcfd9f66401d6
Serafeim Maroulis <seramak@gmail.com>
2014-05-19 13:48:06 +0100

    Books are now being displayed

--- javascripts/books.js
+++ javascripts/books.js
@@ -1,3 +1,19 @@
 function handleResponse(response) {
   console.log(response.items);
+
+
+  _.each(response.items, function(book) {
+
+    var img_url = book.volumeInfo.imageLinks.thumbnail;
+    var bookCon = document.getElementById('bookshelf');
+    var image = document.createElement("img");
+    image.setAttribute('src',img_url);
+    bookCon.appendChild(image);
+
+  });
+
 }
+
+
+
+// response.items[0].volumeInfo.imageLinks.thumbnail;
