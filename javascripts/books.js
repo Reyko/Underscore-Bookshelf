a093ddd56e8e18b6cd6214bac81fc0d74e699028
Serafeim Maroulis <seramak@gmail.com>
2014-06-07 23:08:14 +0100

    star rating added

--- javascripts/books.js
+++ javascripts/books.js
@@ -7,13 +7,72 @@ function handleResponse(response) {
     var img_url = book.volumeInfo.imageLinks.thumbnail;
     var bookCon = document.getElementById('bookshelf');
     var image = document.createElement("img");
+    
+    var container = document.createElement("div");
+    container.setAttribute('class',"wrapper");
+
+    var star = document.createElement("div");
+    star.setAttribute("class","hot");
+    var starImage = document.createElement("img");
+    
+    starImage.src = "img/star.png";
+
+    var averageRating = book.volumeInfo.averageRating;
+
+    if (averageRating >= 4){
+      star.appendChild(starImage);
+    }
+
     image.setAttribute('src',img_url);
-    bookCon.appendChild(image);
+    container.appendChild(image);
+    container.appendChild(star);
+
+    bookCon.appendChild(container);
 
   });
 
 }
 
+
+"We' ll call this constructor"
+
+// function Book(book_info) {
+//   this.image_url = book_info.volumeInfo.imageLinks.thumbnail;
+//   this.title = book_info.volumeInfo.title;
+//   this.rating = book.volumeInfo.averageRating;
+// }
+
+
+// Book.prototype.add_to_bookshelf = function(){
+
+//   var image = $("img");
+//   image.attr("src")
+
+
+// }
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+
+//   var callback = function(event) {
+//     console.log("Hello");
+//   };
+
+//   $('img').click(callback);
+
+//   $('search-box').change(function(){
+
+//   });
+
+
+
+// })
 
 
 // response.items[0].volumeInfo.imageLinks.thumbnail;
