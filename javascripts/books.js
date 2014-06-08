64af97f676ea974f6e9f1c50dca978d3146a9db8
Serafeim Maroulis <seramak@gmail.com>
2014-06-08 15:14:18 +0100

    search feature fixed

--- javascripts/books.js
+++ javascripts/books.js
@@ -1,21 +1,28 @@
-function handleResponse(response) {
-  console.log(response.items);
+function handleResponse(response,title) {
 
-  _.each(response.items, function(book) {
-
-    new Book(book).add_to_bookshelf();
-
+  $( "#searchbox" ).submit(function( event ) {
+    var title = document.getElementById("search").value.toLowerCase();
+    $(".wrapper").remove();
+    handleResponse(response,title);
+    event.preventDefault();
   });
 
+  console.log(response.items);
+  console.log(title);
+
+  _.each(response.items, function(book) {
+    var book = new Book(book);
+    bookTitle = book.title.toLowerCase();
+    if (title!==null && book.title.toLowerCase() === title){
+      book.add_to_bookshelf();
+    }
+    else if(title==null){
+      book.add_to_bookshelf();
+    }
+  });
 }
 
-function hello() {
-    var val = document.getElementById("search").value;
-    var findByTitle = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
-    alert(val);
-}
 
-"We' ll call this constructor"
 
 function Book(book_info) {
   this.image_url = book_info.volumeInfo.imageLinks.thumbnail;
@@ -24,52 +31,24 @@ function Book(book_info) {
 }
 
 
-Book.prototype.add_to_bookshelf = function(){
+Book.prototype.add_to_bookshelf = function(title){
+  var bookCon = document.getElementById('bookshelf');
+  var image = document.createElement("img");
 
-    var bookCon = document.getElementById('bookshelf');
-    var image = document.createElement("img");
+  var container = document.createElement("div");
+  container.setAttribute('class',"wrapper");
 
-    var container = document.createElement("div");
-    container.setAttribute('class',"wrapper");
+  var star = document.createElement("div");
+  star.setAttribute("class","hot");
+  var starImage = document.createElement("img");
 
-    var star = document.createElement("div");
-    star.setAttribute("class","hot");
-    var starImage = document.createElement("img");
+  starImage.src = "img/star.png";
 
-    starImage.src = "img/star.png";
+  _.filter([this.rating], function(num){ if (num > 3){ return star.appendChild(starImage);}});
+    
+  image.setAttribute('src',this.image_url);
 
-    _.filter([this.rating], function(num){ if (num > 3){ return star.appendChild(starImage);}});
-      
-   
-
-    image.setAttribute('src',this.image_url);
-
-
-    container.appendChild(image);
-    container.appendChild(star);
-    bookCon.appendChild(container);
-
+  container.appendChild(image);
+  container.appendChild(star);
+  bookCon.appendChild(container);
 }
-
-
-
-
-
-// $(document).ready(function(){
-
-//   var callback = function(event) {
-//     console.log("Hello");
-//   };
-
-//   $('img').click(callback);
-
-//   $('search-box').change(function(){
-
-//   });
-
-
-
-// })
-
-
-// response.items[0].volumeInfo.imageLinks.thumbnail;
